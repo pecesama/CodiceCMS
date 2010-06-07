c02e0e53d51de7623f77829d45bbd27c9ec1645a
Victor De la Rocha <vyk2rr@gmail.com>
2010-06-06 17:44:00 -0700

    added themeExists();

--- app/libs/utils.class.php
+++ app/libs/utils.class.php
@@ -33,4 +33,12 @@ class utils{
 			return nl2br($comment);
 		}	
 	}
+
+	public function themeExists($name){
+		if(file_exists(Absolute_Path."app".DIRSEP."themes".DIRSEP.$name) === false){
+			return false;
+		}else{
+			return true;
+		}
+}
 }
