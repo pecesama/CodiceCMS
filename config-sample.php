c5c11f6e2c860feb9e649643bb662ee29d3e6925
Victor De la Rocha <vyk2rr@gmail.com>
2010-09-04 23:01:18 -0500

    Added a config-sample.php and a list of ignored files...

---
+++ config-sample.php
@@ -0,0 +1,10 @@
+<?php
+define('requiresBD', true);
+define('DB_Engine', 'mysqli');
+define('DB_Server', '');
+define('DB_User', '');
+define('DB_Password', '');
+define('DB_name', '');
+define('DB_Port', false);
+define('Path', "http://localhost/path_to_codice_cms_dir");
+?>
