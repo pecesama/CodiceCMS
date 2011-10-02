37325717e730093f503871f1f87477b04f44427b
Victor De la Rocha <vyk2rr@gmail.com>
2011-10-02 18:14:27 -0500

    Minor changes, approaching to the new control panel style

--- app/views/comments/edit.php
+++ app/views/comments/edit.php
@@ -1,84 +1,84 @@
-	<?php echo $this->renderElement("admin_header"); ?>
 
-	<div id="page-navigation" class="clearfix">
-			<ul>
-				<li>
-					<?php echo $this->html->linkTo("Administrar posts", "admin/", " title=\"Administrar los posts\""); ?>
-				</li>
-				<li class="current">
-					<?php echo $this->html->linkTo("Administrar comentarios", "comments/", " title=\"Administrar los comentarios\""); ?>
-				</li>
-				<li>
-					<?php echo $this->html->linkTo("Agregar post", "admin/add/", " title=\"Agregar un nuevo post\""); ?>
-				</li>
-				<li>
-					<?php echo $this->html->linkTo("Configuraci&oacute;n", "admin/config/", " title=\"Configurar blog\""); ?>
-				</li>
-				<li>
-					<?php echo $this->html->linkTo("Cerrar sesi&oacute;n", "admin/logout/", " title=\"Terminar la sesi&oacute;n\""); ?>
-				</li>
-				<li>
-					<?php echo $this->html->linkTo("Ir al blog", "", " title=\"Regresar al blog\""); ?>
-				</li>
-			</ul>
-				
-		</div>
+<div class="row">
+    <div class="span12">
+		<?php echo $this->html->form("comments/edit/".$id."/"); ?>
+			<div class="clearfix">
+				<label for="status">Publishing status</label>
+				<div class="input">
+					<?php echo $this->html->select("status", $statuses, $comment["status"]); ?>
+				</div>
+			</div>
 
-		<div id="page-content" class="clearfix"> 
+			<div class="clearfix">
+				<label for="author">Comment author</label>
+				<div class="input">
+					<?php echo $this->html->textField("author"," value=\"{$comment["author"]}\" class='xlarge'");?>
+				</div>
+			</div>
 
-			<h1>Codice CMS Dashboard</h1> 
+			<div class="clearfix">
+				<label for="email">Email</label>
+				<div class="input">
+					<?php echo $this->html->textField("email"," value=\"{$comment["email"]}\"");?>
+				</div>
+			</div>
 
-			<h2>Editar comentario:</h2> 
-			<div class="inner-box clearfix">
+			<div class="clearfix">
+				<label for="url">Web page</label>
+				<div class="input">
+					<?php echo $this->html->textField("url"," value=\"{$comment["url"]}\" class='xlarge'");?>
+				</div>
+			</div>
 
-				<div id="form-block"> 
+			<div class="clearfix">
+            	<div class="input">
+              		<ul class="inputs-list">
+                		<li>
+                			<label>
+                				<?php $checked = $comment["suscribe"]?"checked=\"true\"":""; ?>
+								<?php echo $this->html->checkbox("suscribe",$checked);?> Suscribed to comments.</span>
+                  			</label>
+                		</li>
+                	</ul>
+                </div>
+            </div>
 
-					<?php echo $this->html->form("comments/edit/".$id."/"); ?>
+            <div class="clearfix">
+				<label>View post</label>
+				<div class="input">
+					<?php echo $this->html->linkTo("Go to: {$comment["post"]["title"]}", $comment["post"]["urlfriendly"]."/", " title=\"View posst\" target=\"_blank\" class='btn'"); ?>
+				</div>
+			</div>
 
-						<label for="status">Estado</label>
-						<?php echo $this->html->select("status", $statuses, $comment["status"]); ?>
+			<p>
+				<strong>IP</strong>
+				<?php echo $comment["IP"]; ?>
+			</p>
 
-						<label for="author">Autor</label>
-						<?php echo $this->html->textField("author"," value=\"{$comment["author"]}\"");?>
+			<p>
+				<strong>created</strong>
+				<?php echo $comment["created"]; ?>
+			</p>
 
-						<label for="email">Correo</label>
-						<?php echo $this->html->textField("email"," value=\"{$comment["email"]}\"");?>
+			<p>
+				<strong>modified</strong>
+				<?php echo $comment["modified"]; ?>
+			</p>
 
-						<label for="url">URL</label>
-						<?php echo $this->html->textField("url"," value=\"{$comment["url"]}\"");?>
-
-						<label for="suscribe">Suscrito a comentarios</label>
-						<?php
-							$checked = $comment["suscribe"]?"checked=\"true\"":"";
-							echo $this->html->checkbox("suscribe",$checked);
-						?>
-
-						<label>View post</label>
-						<?php echo $this->html->linkTo("{$comment["post"]["title"]}", $comment["post"]["urlfriendly"]."/", " title=\"View posst\" target=\"_blank\""); ?>
-
-						<label>IP</label>
-						<?php echo $comment["IP"]; ?>
-
-						<label>created</label>
-						<?php echo $comment["created"]; ?>
-
-						<label>modified</label>
-						<?php echo $comment["modified"]; ?>
-
-						<label for="content">Contenido</label>
-						<?php echo $this->html->textArea("content", $comment["content"], " rows=\"3\" cols=\"3\" "); ?>
-
-						<em>Contenido del post</em>
-
-						<input class="submit reset" id="cancelar" name="cancelar" type="submit" value="Cancelar">
-						<input class="submit" type="submit" value="Modificar" />
-
-						</form>
-
-				</div> 
 			
-			</div> 
-		</div>
-	
-	
-	<?php echo $this->renderElement("admin_footer"); ?>
+			<div class="clearfix">
+				<label for="content">Entry content</label>
+				<div class="input">
+				<?php echo $this->html->textArea("content", $comment["content"], " rows=\"3\" class='xxlarge' "); ?>
+				</div>
+			</div>
+			
+			<div class="actions">
+				<input class="btn" id="cancelar" name="cancelar" type="submit" value="Cancel">
+				<input class="btn danger" type="submit" value="Save changes" />
+			</div>
+		</form>
+	</div>
+</div>
+
+<?php echo $this->renderElement("admin_footer"); ?>
--- app/views/comments/index.php
+++ app/views/comments/index.php
@@ -1,105 +1,24 @@
-	<?php echo $this->renderElement("admin_header"); ?>
+<table class="zebra-striped">
+	<thead>
+		<tr>
+			<th>Content</th>
+			<th>Publishing status</th>
+			<th colspan="3">Actions</th>
+		</tr>
+	</thead>
+	<tbody>
+		<?php foreach ($comments as $comment) { ?>
+			<tr id="row<?php echo $comment["ID"];?>">
+				<td><?php echo $comment["content"]; ?></td>
+				<td><?php echo $comment["status"]; ?></td>
+				<td><?php echo $this->html->linkTo("Editar","comments/edit/{$comment["ID"]}"," title=\"{$comment["ID"]}\" class='btn info'"); ?></td>
+				<td><?php echo $this->html->linkTo("Borrar","comments/remove/{$comment["ID"]}"," title=\"{$comment["ID"]}\" class='btn danger'"); ?></td>
+				<td><?php echo $this->html->linkTo("Aprobar","comments/approve/{$comment["ID"]}"," title=\"{$comment["ID"]}\" class='btn success'"); ?></td>
+			</tr>
+		<?php } ?>
+	</tbody>
+</table>
 
-	<div id="page-navigation" class="clearfix">
-			<ul>
-				<li>
-					<?php echo $this->html->linkTo("Administrar posts", "admin/", " title=\"Administrar los posts\""); ?>
-				</li>
-				<li class="current">
-					<?php echo $this->html->linkTo("Administrar comentarios", "comments/", " title=\"Administrar los comentarios\""); ?>
-				</li>
-				<li>
-					<?php echo $this->html->linkTo("Agregar post", "admin/add/", " title=\"Agregar un nuevo post\""); ?>
-				</li>
-				<li>
-					<?php echo $this->html->linkTo("Configuraci&oacute;n", "admin/config/", " title=\"Configurar blog\""); ?>
-				</li>
-				<li>
-					<?php echo $this->html->linkTo("Cerrar sesi&oacute;n", "admin/logout/", " title=\"Terminar la sesi&oacute;n\""); ?>
-				</li>
-				<li>
-					<?php echo $this->html->linkTo("Ir al blog", "", " title=\"Regresar al blog\""); ?>
-				</li>
-			</ul>
+<?php echo $pagination; ?>
 				
-		</div>
-
-		<div id="page-content" class="clearfix">
-
-			<h1>Codice CMS Dashboard</h1>
-
-			<h2>Panel de administraci&oacute;n<span> (editar y eliminar)</span></h2>
-			<div class="inner-box clearfix">
-
-				<div id="sidebar">
-
-					<ul>
-						<li class="head">Herramientas</li>
-						<li>
-							<?php echo $this->html->linkTo("Posts", "admin/", " title=\"Administrar los posts\""); ?>
-						</li>
-						<li class="current">
-							<?php echo $this->html->linkTo("Comentarios", "comments/", " title=\"Administrar los comentarios\""); ?>
-						</li>
-						<li>
-							<?php echo $this->html->linkTo("Enlaces", "admin/comments/", " title=\"Administrar los enlaces\""); ?>
-						</li>
-					</ul>
-
-				</div>
-
-				<div id="table-block">
-
-					<table cellspacing="0" cellpadding="0">
-					<tbody>
-						<?php
-						if ($comments == null) {
-						?>
-							<div class="error">
-								No hay comentarios
-							</div>
-						<?php
-						} else {
-						?>
-							<tr class="header">
-								<td>Contenido</td>
-								<td>Estado</td>
-								<td>Acciones</td>
-							</tr>
-						<?php
-							$odd = false;
-							foreach ($comments as $comment) { 
-								$alt = ($odd) ? "class=\"alternate\"" : "" ;
-								$status = ($comment["status"] == "publish") ? "<td class=\"true\">published</td>" : "<td class=\"false\">waiting</td>" ;
-						?>
-							<tr <?php echo $alt; ?> id="reg<?php echo $comment["ID"];?>">
-								<td>
-									<?php echo $comment["content"]; ?><br>
-									<strong><?php echo $this->l10n->__("View post");?></strong>:
-									<?php echo $this->html->linkTo("\"{$comment["post"]["title"]}\"", $comment["post"]["urlfriendly"]."/", " title=\"View posst\" target=\"_blank\""); ?>
-								</td>
-								<?php echo $status; ?>
-								<td class="actions">
-									<?php
-										echo $this->html->linkTo("Editar","comments/edit/{$comment["ID"]}","class=\"edit\" title=\"{$comment["ID"]}\"");
-										echo $this->html->linkTo("Borrar","comments/remove/{$comment["ID"]}","class=\"remove\" title=\"{$comment["ID"]}\"");
-										echo $this->html->linkTo("Aprobar","comments/approve/{$comment["ID"]}","class=\"approve\" title=\"{$comment["ID"]}\"");
-									?>
-								</td>
-							</tr>
-						<?php
-								$odd = !$odd;
-							} 
-						} 
-						?>
-					</tbody>
-				</table>
-
-				<?php echo $pagination; ?>
-				</div>
-
-			</div>
-
-		</div>
-
-	<?php echo $this->renderElement("admin_footer"); ?>
+<?php echo $this->renderElement("admin_footer"); ?>
