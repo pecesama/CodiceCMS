7eafad4a4208cca3fe1c8bf1e1e7be1430733d75
Victor De la Rocha <vyk2rr@gmail.com>
2011-10-04 13:06:36 -0500

    Minor changes

--- app/views/elements/posts.php
+++ app/views/elements/posts.php
@@ -7,31 +7,30 @@ div.test_ > * > *{
 </style>
 <?php if(count($posts) > 0){?>
 	<?php foreach($posts as $post){?>
-		<div class="row test">
+		<div class="row">
+			<h3><?php echo $this->html->linkTo($post["title"],$post["urlfriendly"],'rel="bookmark" title="Enlace a '.$post["title"].'"'); ?></h3>
+			
+			<?php echo $post["content"]; ?>
+			
 			<div class="row">
-				<div class="span"><h3><?php echo $this->html->linkTo($post["title"],$post["urlfriendly"],'rel="bookmark" title="Enlace a '.$post["title"].'"'); ?></h3></div>
+				<div class="span">Escrito por <?php echo $post["autor"]["name"]; ?> el <?php echo $post["created"]?></div>
+				<div class="span">
+					<?php if($post["tags"]){ ?>
+						<?php foreach($post["tags"] as $tag){ ?>
+							<?php echo $this->html->linkTo($tag["tag"],"tag/{$tag["urlfriendly"]}"); ?>
+						<?php }?>
+					<?php } ?>
+				</div>
+				<div class="span"><?php echo $this->html->linkTo($post["comments_count"] . " Comentarios","{$post["urlfriendly"]}#comments",'rel="bookmark" title="Comentarios de '.$post["title"].'"'); ?> &#187;</div>
 				<div class="span facebook"><fb:like href="http://mis-algoritmos.com/<?php echo $post['urlfriendly']; ?>" layout="button_count"></fb:like></div>
 			</div>
 
-			<div class="row">
-				<?php echo $post["content"]; ?>
-			</div>
-			
-			
 			<?php if(isset($busqueda) === false){ ?>
-					<p><?php echo $this->html->linkTo($post["comments_count"],"{$post["urlfriendly"]}#comments",'rel="bookmark" title="Comentarios de '.$post["title"].'"'); ?> &#187;</p>
 					
-					<p><?php if($post["tags"]){ ?>
-						<p class="tags">
-							Tags:
-							<?php foreach($post["tags"] as $tag){ ?>
-								<?php echo $this->html->linkTo($tag["tag"],"tag/{$tag["urlfriendly"]}"); ?>
-							<?php }//foreach ?>
-							<br />
-						</p>
-					<?php } ?></p>
+					
+					
 				
-					<p>Escrito por <?php echo $post["autor"]["name"]; ?> el <?php echo $post["created"]?></p>
+					
 				
 			<?php }else{ ?>
 
