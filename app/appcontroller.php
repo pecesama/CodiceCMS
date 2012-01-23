f0fd19caf20b4fba17ab768fc2b233d21a4d8cc2
Aaron Munguia <aamm89@gmail.com>
2012-01-22 18:11:32 -0600

    Add messages instance to appcontroller and appviews

--- app/appcontroller.php
+++ app/appcontroller.php
@@ -6,15 +6,21 @@ abstract class appcontroller extends controller {
 	protected $plugin;
 	protected $User;
 	protected $config;
+        protected $messages;
 
 	public function __construct(){
 		parent::__construct();
+                
+                $this->messages = Message::getInstance();
+                
 		$this->registry = registry::getInstance();
 		
 		$this->html = html::getInstance();
 		$this->plugin = plugin::getInstance();
 		$this->User = User_lib::getInstance();
-
+                
+                
+                
 		//loads configuration of the logged user and blog.
 		$C = new configuration();
 		$this->config = array(
--- app/appviews.php
+++ app/appviews.php
@@ -3,9 +3,13 @@
 class appviews extends views {
 
 	protected $User;
+        protected $messages;
 
 	public function __construct() {
 		parent::__construct();
+                
+                $this->messages = Message::getInstance();
+                
 		$this->User = User_lib::getInstance();
 	}
 	
