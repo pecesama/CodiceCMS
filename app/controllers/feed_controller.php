b03b92950cdb4eaf752ff72a3ebf7fbd09ae9483
Aaron Munguia <aamm89@gmail.com>
2012-07-25 13:13:17 -0500

    Fixbug: Feed RSS chance table fields

--- app/controllers/feed_controller.php
+++ app/controllers/feed_controller.php
@@ -12,17 +12,24 @@ class feed_controller extends appcontroller {
 	
 	public function rss($id=NULL){
 		$this->plugin->call('feed_header');
-
+		
 		$post = new post();
 
 		$this->view->setLayout("feed");
-		$posts = $post->findAll("ID,urlfriendly,title,IF(POSITION('<!--more-->' IN content)>0,MID(content,1,POSITION('<!--more-->' IN content)-1),content) as content, created", "ID DESC", $this->config["blog"]['blog_posts_per_page'], "WHERE status = 'publish'");
+		$posts = $post->findAll("idPost,urlfriendly,title,IF(POSITION('<!--more-->' IN content)>0,MID(content,1,POSITION('<!--more-->' IN content)-1),content) as content, created", "idPost DESC", $this->config["blog"]['blog_posts_per_page'], "WHERE idStatus = '1'");
 		$temp = array();
 		foreach($posts as $a_post){
-			$temp[$a_post['ID']] = $a_post;
-			$temp[$a_post['ID']]['tags'] = $post->getTags($a_post['ID'],'string');
+			$temp[$a_post['idPost']] = $a_post;
+			$temp[$a_post['idPost']]['tags'] = $post->getTags($a_post['idPost'],'string');
 		}
 		$this->view->posts = $temp;
+		
+		// Get configuration
+/*		$configuration = new configuration();
+		$this->view->conf = $configuration->findAll();
+	*/
+
+		
 		$this->render("rss");
 	}
 }
